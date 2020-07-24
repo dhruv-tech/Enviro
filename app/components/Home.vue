a1c8bc2ed0d491c2782324642863bad0e5b1f999
Dhruv <26849655+dhruv-tech@users.noreply.github.com>
2020-07-25 03:17:28 +0530

    progress -home

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,41 +1,52 @@
 <template>
-    <Page>
-        <ActionBar>
-            <Label text="Home"/>
-        </ActionBar>
+    <Page actionBarHidden="true">
+      <ScrollView orientation="vertical">
+        <StackLayout orientation="vertical" class="center-text">
+          <Label text="Hyderabad" class="title"/>
 
-        <GridLayout>
-            <Label class="info">
-                <FormattedString>
-                    <Span class="fas" text.decode="&#xf135; "/>
-                    <Span :text="message"/>
-                </FormattedString>
-            </Label>
-        </GridLayout>
+          <FlexboxLayout flexDirection="column" backgroundColor="#ffffff" id="title-card" class="span-85 card">
+            
+          </FlexboxLayout>
+
+          <GridLayout rows="auto" columns="145" id="status-img-container">
+            <Image src="~/components/imgs/satisfactory.png" stretch="aspectFit" id="status-img"/>
+          </GridLayout>
+
+          <Label text="For You" class="subtitle pull-up"/>
+          <FlexboxLayout flexDirection="row" justifyContent="center" class="span-85 card circle-center">
+            <StackLayout class="circle" backgroundColor="#ffffff">
+              
+            </StackLayout>
+            <StackLayout class="spacer-x"></StackLayout>
+            <StackLayout class="circle" backgroundColor="#ffffff">
+              
+            </StackLayout>
+            <StackLayout class="spacer-x"></StackLayout>
+            <StackLayout class="circle" backgroundColor="#ffffff">
+              
+            </StackLayout>
+          </FlexboxLayout>
+
+          <Label text="Trends" class="subtitle"/>
+          <FlexboxLayout flexDirection="column" backgroundColor="#ffffff" class="span-85 card">
+            
+          </FlexboxLayout>
+
+          <Label text="Air Composition" class="subtitle"/>
+          <FlexboxLayout flexDirection="column" backgroundColor="#ffffff" class="span-85 card">
+            
+          </FlexboxLayout>
+
+          <FlexboxLayout justifyContent="center" id="footer-logo">
+            <GridLayout rows="auto" columns="135">
+              <Image src="~/components/imgs/logo-long.png" stretch="aspectFit"/>
+            </GridLayout>
+          </FlexboxLayout>
+          <FlexboxLayout justifyContent="center" id="footer-text">
+            <Label text="Version 1.0.0 / Android / English"/>
+          </FlexboxLayout>
+
+        </StackLayout>
+      </ScrollView>
     </Page>
 </template>
-
-<script>
-  export default {
-    computed: {
-      message() {
-        return "Blank {N}-Vue app";
-      }
-    }
-  };
-</script>
-
-<style scoped lang="scss">
-    @import '~@nativescript/theme/scss/variables/blue';
-
-    // Custom styles
-    .fas {
-        @include colorize($color: accent);
-    }
-
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
-    }
-</style>
